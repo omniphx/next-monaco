7169f2e8b13216d4c80e6307dd0c1f6ff1f3f96b
Marty Mitchener <marty@patterns.app>
2022-10-26 14:49:00 -0700

    Initialize with code example

---
+++ components/Code.tsx
@@ -0,0 +1,24 @@
+import Editor from '@monaco-editor/react';
+
+import loader from '@monaco-editor/loader';
+import { useEffect } from 'react';
+
+
+const Code = () => {
+
+  useEffect(() => {
+    loader.init().then((monaco) => {
+      console.log("Here is the monaco instance", monaco);
+    });
+  })
+
+  return (
+    <Editor
+      height="90vh"
+      defaultLanguage="javascript"
+      defaultValue="// let's write some broken code 😈"
+    />
+  )
+};
+
+export default Code;
